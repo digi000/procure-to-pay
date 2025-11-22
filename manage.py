ba43cfd35e62dda8898da832d800b3b7e7187df9
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-22 22:20:27 +0200

    Add backend directory to Python path

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
+    backend_dir = Path(__file__).resolve().parent / 'backend'
+    sys.path.insert(0, str(backend_dir))
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
