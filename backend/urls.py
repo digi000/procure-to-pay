85a8a04c12076336af60549f577b95e0ec90769b
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-24 18:32:09 +0200

    feat: Integrate purchases app URLs and serve media in debug

--- backend/urls.py
+++ backend/urls.py
@@ -23,4 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.users.urls')),  # Include user app URLs
+    path('', include('apps.purchases.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
