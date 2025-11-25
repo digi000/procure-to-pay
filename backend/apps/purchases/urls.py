7236255c7e0fb92cf735d140dbbe712deed86352
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-25 18:17:47 +0200

    feat: allow trailing slash for purchase urls

--- backend/apps/purchases/urls.py
+++ backend/apps/purchases/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import PurchaseRequestViewSet
 
-router = DefaultRouter(trailing_slash=False)
+router = DefaultRouter()
 router.register(r'requests', PurchaseRequestViewSet, basename='purchase-request')
 
 urlpatterns = [
