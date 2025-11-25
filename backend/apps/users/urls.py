b0953a4cdfcca2d91a90e5c55b6c30ab15eb368f
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-25 18:09:50 +0200

    remove trailing slash from user urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 from .api import AuthViewSet, UserViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'auth', AuthViewSet, basename='auth')
 router.register(r'users', UserViewSet, basename='users')
 
