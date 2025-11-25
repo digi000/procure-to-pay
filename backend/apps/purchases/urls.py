15276cc6c595ba1ea079b0a36a7a40f2e431690d
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-11-25 18:09:48 +0200

    remove trailing slash from purchase urls

--- backend/apps/purchases/urls.py
+++ backend/apps/purchases/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import PurchaseRequestViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'requests', PurchaseRequestViewSet, basename='purchase-request')
 
 urlpatterns = [
